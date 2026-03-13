ef0ac706bebf6572fbeea5090fc8fab556445e94
Priyanka081503 <gowdapriyanka332@gmail.com>
2026-03-13 15:57:03 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,45 +1,11 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
-import cv2
-import numpy as np
+from fastapi import FastAPI, WebSocket
 
 app = FastAPI()
 
-total_people = 0
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
 
-@app.get("/")
-def home():
-    return {"message": "AI Camera Server Running"}
-
-@app.post("/detect")
-async def detect(file: UploadFile = File(...)):
-    global total_people
-
-    image_bytes = await file.read()
-    np_arr = np.frombuffer(image_bytes, np.uint8)
-    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-    # temporary detection
-    persons = 1
-
-    total_people += persons
-
-    return {
-        "persons": persons,
-        "total_people_today": total_people
-    }
-
-@app.get("/dashboard", response_class=HTMLResponse)
-def dashboard():
-    html = f"""
-    <html>
-    <head>
-        <title>AI Camera Dashboard</title>
-    </head>
-    <body>
-        <h1>AI Camera Dashboard</h1>
-        <h2>Total People Detected Today: {total_people}</h2>
-    </body>
-    </html>
-    """
-    return html
+    while True:
+        data = await websocket.receive_text()
+        print("Frame received")
